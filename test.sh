8e5f9558a9e162924edec73d9f94f71106b5bdd4
jenks <simojenki@users.noreply.github.com>
2015-07-14 13:31:46 +0100

    adding initial test to assert cleansup correctly

---
+++ test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+tmp=$(mktemp -d)
+echo "Using tmp dir ${tmp}"
+trap "rm -Rf ${tmp}" EXIT
+
+master=${tmp}/master
+dup1=${tmp}/dup1
+dup2=${tmp}/dup2
+
+function mkf() {
+	local content=$1
+	shift
+	while (( "$#" )); do
+		local path=$1
+		mkdir -p $(dirname ${path}) && echo ${content} > ${path}
+		shift
+	done
+}
+
+function assert_e() {
+	while (( "$#" )); do
+		local file=$1	
+		[[ -e "${file}" ]] || { echo "Expected ${file} to exist, but it doesn't" && exit 1; }
+		shift
+	done
+}
+
+mkdir -p ${master} ${dup1} ${dup2} || { echo "failed to create initial paths" && exit 1 ; }
+
+mkf "content1" 	"${master}/file1" \
+		"${master}/dir/file1"
+	
+assert_e "${master}/file1" \
+	 "${master}/dir/file1"
+
+
+
+
